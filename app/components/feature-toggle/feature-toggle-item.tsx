3c08e8d926fe5ffb7f6847d954c03cd5d827fb78
Bashar Al-Rawi <basharal@live.com>
2022-07-11 12:45:03 -0700

    Make feature toggle list item aligned to the left

--- app/components/feature-toggle/feature-toggle-item.tsx
+++ app/components/feature-toggle/feature-toggle-item.tsx
@@ -1,6 +1,8 @@
 import { useRouter } from 'next/router';
 
 import { ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/system';
 
 import { FeatureToggle } from '../../api';
 import { FEATURE_TOGGGLES } from '../../utils/constants';
@@ -12,6 +14,8 @@ export type FeatureTogglesItemProps = {
 
 export const FeatureToggleItem = (props: FeatureTogglesItemProps) => {
   const router = useRouter();
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
   return (
     <ListItem disablePadding>
       <ListItemButton
@@ -22,8 +26,12 @@ export const FeatureToggleItem = (props: FeatureTogglesItemProps) => {
         <ListItemIcon>
           <LiveToggleIcon featureToggle={props.featureToggle}></LiveToggleIcon>
         </ListItemIcon>
-        <ListItemText sx={{ pl: 2 }} primary={props.featureToggle.name!} />
-        <ListItemText secondary={props.featureToggle.description} />
+        <ListItemText
+          sx={{ pl: 2, maxWidth: 300 }}
+          primary={props.featureToggle.name!}
+          primaryTypographyProps={{ noWrap: true }}
+        />
+        {!fullScreen && <ListItemText sx={{ ml: 2 }} secondary={props.featureToggle.description} />}
       </ListItemButton>
     </ListItem>
   );
